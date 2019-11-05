b009bb3791f8a58b02eec6d70279d9dfc5f27edf
luoqi <luoqi@bytedance.com>
2019-11-05 23:56:53 +0800

    lesson 6, quick sort

---
+++ src/main/java/algs4/lesson6/QuickSort.java
@@ -0,0 +1,36 @@
+package algs4.lesson6;
+
+import common.StdRandom;
+
+public class QuickSort {
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (less(a[++i], a[lo])) {
+                if (i == hi) break;
+            }
+            while (less(a[lo], a[--j])) {
+                if (j == lo) break;
+            }
+
+            if (i >= j)  break;
+            exch(a, i, j);
+        }
+
+        exch(a, lo, j);
+        return j;
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo)  return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+}
