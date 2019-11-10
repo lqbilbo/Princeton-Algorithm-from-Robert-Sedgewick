92307cabfd02aafdeb31017f1b3507418e4b4634
luoqi <luoqi@bytedance.com>
2019-11-10 18:28:35 +0800

    lesson 7. priority queue

--- src/main/java/algs4/lesson6/QuickSort.java
+++ src/main/java/algs4/lesson6/QuickSort.java
@@ -7,18 +7,18 @@ public class QuickSort {
     private static int partition(Comparable[] a, int lo, int hi) {
         int i = lo, j = hi + 1;
         while (true) {
-//            while (less(a[++i], a[lo])) {
-//                if (i == hi) break;
-//            }
-//            while (less(a[lo], a[--j])) {
-//                if (j == lo) break;
-//            }
+            while (less(a[++i], a[lo])) {
+                if (i == hi) break;
+            }
+            while (less(a[lo], a[--j])) {
+                if (j == lo) break;
+            }
 
             if (i >= j)  break;
-//            exch(a, i, j);
+            exch(a, i, j);
         }
 
-//        exch(a, lo, j);
+        exch(a, lo, j);
         return j;
     }
 
@@ -36,5 +36,17 @@ public class QuickSort {
         int j = partition(a, lo, hi);
         sort(a, lo, j-1);
         sort(a, j+1, hi);
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp;
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+
+        return false;
     }
 }
