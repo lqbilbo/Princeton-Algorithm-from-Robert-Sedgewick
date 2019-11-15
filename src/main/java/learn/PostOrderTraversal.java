5af82340634d464b525797327d858d3b63fcca99
luoqi <luoqi@bytedance.com>
2019-11-15 11:03:49 +0800

    leetcode learn binary tree

---
+++ src/main/java/learn/PostOrderTraversal.java
@@ -0,0 +1,28 @@
+package learn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PostOrderTraversal {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pollLast();
+            output.addFirst(node.val);
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+        }
+        return output;
+    }
+}
---
+++ src/main/java/learn/PreOrderTraversal.java
@@ -0,0 +1,28 @@
+package learn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PreOrderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+        return output;
+    }
+}
