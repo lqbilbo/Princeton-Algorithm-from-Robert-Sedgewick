5afaa5781eeb0f6ffa7c456cb2e08e36484f8a38
luoqi <luoqi@bytedance.com>
2019-12-05 09:34:38 +0800

    two easy linkedIn problems

---
+++ src/main/java/linkedin/Solution716.java
@@ -0,0 +1,41 @@
+package linkedin;
+
+import java.util.Stack;
+
+public class Solution716 {
+    Stack<Integer> stack;
+    Stack<Integer> maxStack;
+
+    public Solution716() {
+        stack = new Stack();
+        maxStack = new Stack();
+    }
+
+    public void push(int x) {
+        int max = maxStack.isEmpty() ? x : maxStack.peek();
+        maxStack.push(max > x ? max : x);
+        stack.push(x);
+    }
+
+    public int pop() {
+        maxStack.pop();
+        return stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int peekMax() {
+        return maxStack.peek();
+    }
+
+    public int popMax() {
+        int max = peekMax();
+        Stack<Integer> buffer = new Stack();
+        while (top() != max) buffer.push(pop());
+        pop();
+        while (!buffer.isEmpty()) push(buffer.pop());
+        return max;
+    }
+}
