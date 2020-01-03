6db47bf610ef325be493ecfafe3aa7820f46d3a8
luoqi <luoqi@bytedance.com>
2020-01-03 23:01:58 +0800

    two Microsoft problems

---
+++ src/main/java/microsoft/Solution55.java
@@ -0,0 +1,38 @@
+package microsoft;
+
+public class Solution55 {
+
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+
+    enum Index {
+        GOOD, BAD, UNKNOWN
+    }
+
+    public boolean canJumpWithDP(int[] nums) {
+        Index[] memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+
+        return memo[0] == Index.GOOD;
+    }
+}
