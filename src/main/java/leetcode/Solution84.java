47d5630f3a8b1899359464004aae6dd98a0516a9
luoqi <luoqi@bytedance.com>
2020-02-15 23:03:47 +0800

    three Amazon problems

---
+++ src/main/java/leetcode/Solution84.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class Solution84 {
+
+    /**
+     * use stack
+     */
+    public int largestRectangleArea1(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+            stack.push(i);
+        }
+        while (stack.peek() != -1)
+            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() -1));
+        return maxarea;
+    }
+
+    public int calculateArea(int[] heights, int start, int end) {
+        if (start > end)
+            return 0;
+        int minindex = start;
+        for (int i = start; i <= end; i++)
+            if (heights[minindex] > heights[i])
+                minindex = i;
+        return Math.max(heights[minindex] * (end - start + 1), Math.max(calculateArea(heights, start, minindex - 1), calculateArea(heights, minindex + 1, end)));
+    }
+
+    /**
+     * use Divide and Conquer
+     */
+    public int largestRectangleArea2(int[] heights) {
+        return calculateArea(heights, 0, heights.length - 1);
+    }
+}
