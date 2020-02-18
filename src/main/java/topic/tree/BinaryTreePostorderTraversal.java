4ff09d9ff51c42eeb1a77ebe0408e8a0c6d22323
luoqi <luoqi@bytedance.com>
2020-02-18 22:43:21 +0800

    Binary Tree & N-ary Tree problems

---
+++ src/main/java/topic/tree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,30 @@
+package topic.tree;
+
+import learn.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pollLast();
+            output.addFirst(node.val);
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+        }
+        return output;
+    }
+}
---
+++ src/main/java/topic/tree/NaryTreeLevelOrderTraversal.java
@@ -0,0 +1,28 @@
+package topic.tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        List<Node> previousLayer = Arrays.asList(root);
+
+        while (!previousLayer.isEmpty()) {
+            List<Node> currentLayer = new ArrayList<>();
+            List<Integer> previousVals = new ArrayList<>();
+            for (Node node : previousLayer) {
+                previousVals.add(node.val);
+                currentLayer.addAll(node.children);
+            }
+            result.add(previousVals);
+            previousLayer = currentLayer;
+        }
+
+        return result;
+    }
+}
