e7ea37aa08ee82f62996219606191dbd8d227548
luoqi <luoqi@bytedance.com>
2020-02-26 21:35:42 +0800

    two DFS problems

--- src/main/java/leetcode/Solution1.java
+++ src/main/java/leetcode/Solution1.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Solution1 {
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
@@ -15,5 +15,10 @@ public class Solution1 {
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,7,9};
+        twoSum(nums, 10);
     }
 }
