9e11fd0d8a58f5504584ad341319014cc776898c
luoqi <luoqi@bytedance.com>
2020-03-02 23:09:47 +0800

    the 1st week homework

---
+++ src/main/java/com/songying/leetcode/editor/en/MergeSortedArray.java
@@ -0,0 +1,50 @@
+//Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one
+// sorted array. 
+//
+// Note: 
+//
+// 
+// The number of elements initialized in nums1 and nums2 are m and n respectivel
+//y. 
+// You may assume that nums1 has enough space (size that is greater or equal to 
+//m + n) to hold additional elements from nums2. 
+// 
+//
+// Example: 
+//
+// 
+//Input:
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//Output: [1,2,2,3,5,6]
+// Related Topics Array Two Pointers
+
+
+package com.songying.leetcode.editor.en;
+public class MergeSortedArray{
+    public static void main(String[] args) {
+        Solution solution = new MergeSortedArray().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        // while there are still elements to compare
+        while ((p1 >= 0) && (p2 >= 0))
+            // compare two elements from nums1 and nums2
+            // and add the largest one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
