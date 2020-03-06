d130b4af3fbc209c2cf0b68a1d4251187de905ae
luoqi <luoqi@bytedance.com>
2020-03-06 22:28:38 +0800

    finish the 3rd week homework

---
+++ src/main/java/com/songying/leetcode/editor/en/SearchInRotatedSortedArray.java
@@ -0,0 +1,53 @@
+//Suppose an array sorted in ascending order is rotated at some pivot unknown to
+// you beforehand. 
+//
+// (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]). 
+//
+// You are given a target value to search. If found in the array return its inde
+//x, otherwise return -1. 
+//
+// You may assume no duplicate exists in the array. 
+//
+// Your algorithm's runtime complexity must be in the order of O(log n). 
+//
+// Example 1: 
+//
+// 
+//Input: nums = [4,5,6,7,0,1,2], target = 0
+//Output: 4
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [4,5,6,7,0,1,2], target = 3
+//Output: -1 
+// Related Topics Array Binary Search
+
+
+package com.songying.leetcode.editor.en;
+public class SearchInRotatedSortedArray{
+    public static void main(String[] args) {
+        Solution solution = new SearchInRotatedSortedArray().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+                else start = mid + 1;
+            } else {
+                if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+                else end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
