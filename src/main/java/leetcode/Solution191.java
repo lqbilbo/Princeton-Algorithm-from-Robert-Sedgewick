bbb88b008b580ef4b03b76ea94077a1f525356e2
luoqi <luoqi@bytedance.com>
2020-03-25 22:19:22 +0800

    three bit problems

---
+++ src/main/java/leetcode/Solution191.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Solution191 {
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
