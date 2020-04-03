cb7ece3c686b3773f1c6ab83ee831a3e709b6079
luoqi <luoqi@bytedance.com>
2020-04-03 09:42:19 +0800

    some hard dp problems

---
+++ src/main/java/leetcode/Solution300.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution300 {
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+}
